cb676abd608a98d5e99be26abdf1fa7b47caec1a
anzharnugraha26 <anzharnugraha50@gmail.com>
2021-12-08 10:47:05 +0700

    update

--- application/views/admin/invoices.php
+++ application/views/admin/invoices.php
@@ -1,6 +1,6 @@
 <div id="layoutSidenav_content">
 	<main>
-		<div class="container-fluid mt-4" >
+		<div class="container-fluid mt-4">
 			<div class="row">
 				<div class="col">
 					<div class="card">
@@ -8,27 +8,32 @@
 							<h3>Invoice Pemesanan Produk</h3>
 						</div>
 						<div class="card-body">
-							<table class="table table-bordered table-hover table-striped">
-								<tr>
-									<th>Id Invoice</th>
-									<th>Nama Pemesan</th>
-									<th>Alamat Pengiriman</th>
-									<th>Tanggal Pemesanan</th>
-									<th>Batas Pembayaran</th>
-									<th>Aksi</th>
-								</tr>
-								<?php foreach($invoice as $inv): ?>
-								<tr>
-									<td><?php echo $inv->id ?></td>
-									<td><?php echo $inv->nama ?></td>
-									<td><?php echo $inv->alamat ?></td>
-									<td><?php echo $inv->tanggal_pesan ?></td>
-									<td><?php echo $inv->batas_bayar ?></td>
-									<td>
-										<a href="">Detail</a>
-									</td>
-								</tr>
-								<?php endforeach; ?>
+							<table id="datatablesSimple">
+								<thead>
+									<tr>
+										<th>Id Invoice</th>
+										<th>Nama Pemesan</th>
+										<th>Alamat Pengiriman</th>
+										<th>Tanggal Pemesanan</th>
+										<th>Batas Pembayaran</th>
+										<th>Aksi</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php foreach ($invoice as $inv) : ?>
+										<tr>
+											<td><?php echo $inv->id ?></td>
+											<td><?php echo $inv->nama ?></td>
+											<td><?php echo $inv->alamat ?></td>
+											<td><?php echo $inv->tanggal_pesan ?></td>
+											<td><?php echo $inv->batas_bayar ?></td>
+											<td>
+												<a href="">Detail</a>
+											</td>
+										</tr>
+									<?php endforeach; ?>
+								</tbody>
+
 							</table>
 						</div>
 					</div>
